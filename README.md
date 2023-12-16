f5c36cce57077039143f8ac2ec5acc643ede2dcf
Aswin110 <aswinashok110@gmail.com>
2023-12-16 14:14:07 +0530

    added genre details page which will show the books assossiated with the genre

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # local-library
 
  a very basic Express website developed as a tutorial example on the Mozilla Developer Network. (https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs)
+
+ ## Genre details
+
+ We first use Genre.findById() to get Genre information for a specific ID, and Book.find() to get all books records that have that same associated genre ID. 
+
+ check if the genre exists in the data base(it may have been deleted) in this case we will display not found page.
+
+ using description list tag (dl) displayed the books with same genre ID in genre_detail.jade.
--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,4 +1,5 @@
 const Genre = require('../models/genre');
+const Book = require('../models/book');
 const asyncHandler = require('express-async-handler');
 
 exports.genre_list = asyncHandler(async (req, res, next) => {
@@ -10,34 +11,44 @@ exports.genre_list = asyncHandler(async (req, res, next) => {
 });
 
 exports.genre_detail = asyncHandler(async (req, res, next) => {
-	res.send(`NOT IMPLEMENTED: Genre detail: ${req.params.id}`);
+	const [genre, bookInGenre] = await Promise.all([
+		Genre.findById(req.params.id).exec(),						// to get the genre id
+		Book.find({genre: req.params.id}, 'title summary').exec(),	//  to get all books records that have that same associated genre ID.
+	]);
+
+	if (genre === null) {
+		const err = new Error('Genre not found');
+		err.status = 404;
+		return next(err);
+	}
+
+	res.render('genre_detail', {
+		title: 'Genre Detail',
+		genre: genre,
+		genre_books: bookInGenre,
+	});
 });
 
 exports.genre_create_get = asyncHandler(async (req, res, next) => {
 	res.send('NOT IMPLEMENTED: Genre create GET');
 });
 
-// Handle Genre create on POST.
 exports.genre_create_post = asyncHandler(async (req, res, next) => {
 	res.send('NOT IMPLEMENTED: Genre create POST');
 });
 
-// Display Genre delete form on GET.
 exports.genre_delete_get = asyncHandler(async (req, res, next) => {
 	res.send('NOT IMPLEMENTED: Genre delete GET');
 });
 
-// Handle Genre delete on POST.
 exports.genre_delete_post = asyncHandler(async (req, res, next) => {
 	res.send('NOT IMPLEMENTED: Genre delete POST');
 });
 
-// Display Genre update form on GET.
 exports.genre_update_get = asyncHandler(async (req, res, next) => {
 	res.send('NOT IMPLEMENTED: Genre update GET');
 });
 
-// Handle Genre update on POST.
 exports.genre_update_post = asyncHandler(async (req, res, next) => {
 	res.send('NOT IMPLEMENTED: Genre update POST');
 });
